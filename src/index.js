e518a853a8d44d656ffcb0d9770702ada3818688
meowkag <analuiza.aljc@gmail.com>
2023-11-29 01:36:19 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const port = 3000
 
 mongoose.connect('mongodb+srv://meowkag:<password>@cluster0.irnoouy.mongodb.net/?retryWrites=true&w=majority');
 
+const Manga = mongoose.model('Manga', { 
+    titulo: String, 
+    autor: String,
+    genero: String,
+    capitulos: Int,
+});    
+
 app.get('/', (req, res) => {
     res.send("Olá Mundo")
 })
