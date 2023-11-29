f6ae0a224c2b0a34d94ccd1b9548d066b82b3493
meowkag <analuiza.aljc@gmail.com>
2023-11-29 01:24:39 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
 app.get('/', (req, res) => {
     res.send("Olá Mundo")
 })
